d923d09c760e6f3afdcbdcdde552804d272a62e3
qm210 <quantenmace@gmail.com>
2021-06-28 00:54:25 +0200

    web design skills galore

--- papaguy-tamer/app.py
+++ papaguy-tamer/app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from os import listdir
+from time import time, ctime
 
 from . import VERSION
 
 app = Flask(__name__)
 
+basedir = "./papaguy-tamer"
+
+start_time = time()
+
 @app.route('/')
 def index():
-    return f"papaguy-tamer v{VERSION} is running, thanks for checking by."
+    return render_template(
+        'home.html',
+        title=f"papaguy-tamer v{VERSION} is running since {ctime(start_time)}, thanks for checking by.",
+        message=f"qm is wondering: Who the fuck implemented that time string formatting??",
+    )
 
+@app.route('/moves')
+def list_moves():
+    available_moves = listdir(f"{basedir}/moves")
+    return render_template(
+        'list.html',
+        title="Moves",
+        message=f"{len(available_moves)} moves found.",
+        list=available_moves
+    )
