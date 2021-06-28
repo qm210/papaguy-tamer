95b86f2d263f5eec29eb95c2667d4eb95a00b330
qm210 <quantenmace@gmail.com>
2021-06-28 22:45:27 +0200

    tweaking move page

--- papaguy-tamer/app.py
+++ papaguy-tamer/app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, send_from_directory, redirect, url_for
 from time import time, ctime, sleep
 from serial import Serial
 from dataclasses import dataclass, field
-from threading import Thread
+from threading import Thread, Timer
 from playsound import playsound
+from enum import Enum
 import json
 import os
 
@@ -11,10 +12,12 @@ from . import VERSION
 
 app = Flask(__name__)
 
+
 BASE_DIR = "./papaguy-tamer"
 MOVES_DIR = BASE_DIR + "/moves"
 
-start_time = time()
+TIME_RESOLUTION_IN_MS = 50
+server_start_time = time()
 
 @dataclass
 class PapaGuyItself:
@@ -24,11 +27,18 @@ class PapaGuyItself:
 
 papaguy = PapaGuyItself()
 
+known_moves = []
+
+# track.name from BeRos array will be mapped to a short integer ("message name") for the papaguy-itself
+MESSAGE_NAME_MAP = {
+    'head': 1,
+}
+
 @app.route('/')
 def index():
     return render_template(
         'home.html',
-        title=f"papaguy-tamer v{VERSION} is running since {ctime(start_time)}, thanks for checking by.",
+        title=f"papaguy-tamer v{VERSION} is running since {ctime(server_start_time)}, thanks for checking by.",
         message=f"qm is wondering: Who the fuck implemented that time string formatting??",
     )
 
@@ -38,50 +48,57 @@ def favicon():
 
 @app.route('/moves')
 def list_moves():
-    available_moves = get_available_moves()
+    global known_moves
+    known_moves = get_available_moves()
     return render_template(
         'moves.html',
         title="Moves",
-        message=f"{len(available_moves)} moves found.",
-        list=available_moves
+        message=f"{len(known_moves)} moves known.",
+        list=known_moves
     )
 
 def get_available_moves():
     result = []
     for file in os.listdir(MOVES_DIR):
         [name, ending] = file.split(".")
-
         try:
             existing_entry = next((that for that in result if that['id'] == name))
         except StopIteration:
             existing_entry = {'id': name}
             result.append(existing_entry)
-        process_move_entry(existing_entry, file, name, ending)
+        process_move(existing_entry, file, name, ending)
 
-    print(result)
     return result
 
-def process_move_entry(entry, filename, name, ending):
+def process_move(entry, filename, name, ending):
     ending = ending.lower()
 
+    entry['broken'] = entry.get('broken', False)
     if ending in ['wav', 'wave']:
         entry['sample'] = filename
-    elif ending == 'move':
+    elif ending in ['manual', 'auto', 'idle', 'move']:
         try:
             with open(f"{MOVES_DIR}/{filename}") as fp:
                 entry['move'] = json.load(fp)
-        except:
+            entry['trigger'] = ending
+        except json.decoder.JSONDecodeError as err:
             entry['broken'] = True
+            entry.pop('move', None)
+            print("Uh. Broken", err, entry)
     else:
         return
 
-    if not entry.get('broken', False):
+    if not entry['broken'] or 'sample' in entry:
         entry['href'] = '/moves/' + name
+
+    entry['type'] = 'move & sample' if 'sample' in entry and 'move' in entry \
+        else 'move only' if 'move' in entry \
+        else 'sample only' if 'sample' in entry \
+        else '??'
 
 
 @app.route('/moves/<id>')
-def do_move(id=None):
-#    {"tracks": [ {"name":"bla", "automationtimepoints": [ {"time":123,  "value":0.5 }, ... ] }, ... ] }
+def initiate_move(id=None):
     try:
         existing_move = next((move for move in get_available_moves() if move['id'] == id))
     except:
@@ -89,9 +106,17 @@ def do_move(id=None):
 
     if 'sample' in existing_move:
         playsound(MOVES_DIR + '/' + existing_move['sample'], block=False)
-        return f"Playing {existing_move['sample']}"
 
-    return redirect(url_for('list_moves'))
+    if 'move' in existing_move:
+        thread = Thread(target=execute_move, args=(existing_move,))
+        thread.start()
+
+    return f"Executing {existing_move['id']} [{existing_move['type']}]"
+    #return redirect(url_for('list_moves'))
+
+def execute_move(move):
+    print("execute that move, keep track", move)
+    start_time = time()
 
 
 @app.route('/serial/<port>')
@@ -108,13 +133,14 @@ def connect_serial(port=None):
     papaguy.port = port
     papaguy.connection = Serial(port, baudrate=115200, timeout=.1)
 
-    def serial_log():
-        while True:
-            sleep(1)
-            data = papaguy.connection.readline()
-            papaguy.log.append(data)
-            print("DATA:", data)
-
     thread = Thread(target=serial_log)
     thread.start()
     return f"Started on port {papaguy.port}. Have fün."
+
+def serial_log():
+    global papaguy
+    while True:
+        sleep(1)
+        data = papaguy.connection.readline()
+        papaguy.log.append(data)
+        print("DATA:", data)
---
+++ papaguy-tamer/moves/flap.move
@@ -0,0 +1,7 @@
+{"tracks": [ {"name":"wing_left", "automationtimepoints": [
+    {"time":2,  "value":0.25 },
+    {"time":10,  "value":0.5 },
+    {"time":20,  "value":0.65 },
+    {"time":40,  "value":0.75 },
+    {"time":50,  "value":0.85 }
+    ] } ] }
