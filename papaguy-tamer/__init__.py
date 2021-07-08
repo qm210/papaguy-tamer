91805fa2f9110bcb99b2cda4e2668f7b0522e7ef
qm210 <quantenmace@gmail.com>
2021-07-08 20:44:54 +0200

    now actually disable communication

--- papaguy-tamer/__init__.py
+++ papaguy-tamer/__init__.py
@@ -1,7 +1,7 @@
 VERSION = "0.99999"
 PACKAGE_NAME = "papaguy-tamer"
 PRODUCTION = True
-COMMUNICATION_DISABLED = False
+COMMUNICATION_DISABLED = True
 
 BASE_DIR = "./papaguy-tamer"
 MOVES_DIR = BASE_DIR + "/moves"
